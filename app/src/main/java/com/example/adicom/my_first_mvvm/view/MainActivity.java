c6066c8b68cb5719b4ff14cb70968aab1f69e614
adicomdotir <adicom.ir@gmail.com>
2019-08-10 22:28:54 +0430

    Add connection view and view model

--- app/src/main/java/com/example/adicom/my_first_mvvm/view/MainActivity.java
+++ app/src/main/java/com/example/adicom/my_first_mvvm/view/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        UserViewModel userViewModel = UserViewModel()
+        UserViewModel userViewModel = new UserViewModel(this);
+        activityMainBinding.setUser(userViewModel);
     }
 }
